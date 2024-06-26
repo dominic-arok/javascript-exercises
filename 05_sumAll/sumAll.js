f7caf51a9b67083c06816a8fd58113441152681d
Dominic Arokiaraj <dominic.arok@outlook.com>
2024-06-26 13:57:49 -0700

    add functionality that returns the sum of every integer between two given positive integers
    this is what the exercise required

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let errorMessage = 'ERROR';
+    let output = 0;
+
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return errorMessage;
+    }
+    if (num1 < 0 || num2 < 0) {
+        return errorMessage;
+    }
+
+    if (num1 <= num2) {
+        for (let i = num1; i <= num2; i++) {
+            output += i;
+        }
+        return output;
+    }
+    else if (num1 > num2) {
+        for (let i = num1; i >= num2; i--) {
+            output += i;
+        }
+        return output;
+    }
 
 };
 
