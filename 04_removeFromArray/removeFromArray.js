cb061235e6139ad0cdd1c6dda81d348a85896410
Dominic Arokiaraj <dominic.arok@outlook.com>
2024-06-26 13:44:20 -0700

    add functionality that removes given arguments from a given array and returns the array
    this is what the exercise required

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    for (let i = 0; i < args.length; i++) {
+        while(array.indexOf(args[i]) >= 0) {
+            const index = array.indexOf(args[i]);
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
+
 };
 
 // Do not edit below this line
